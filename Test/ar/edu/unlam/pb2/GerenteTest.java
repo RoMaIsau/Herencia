d823d2c1d4e4c388f0164c0b0f81f0937be045be
Macarena Isaurralde <rocio.isau@gmail.com>
2018-09-16 12:54:38 -0300

    sobreescribi el metodo brindar detalles, para el detalle de un gerente

--- Test/ar/edu/unlam/pb2/GerenteTest.java
+++ Test/ar/edu/unlam/pb2/GerenteTest.java
@@ -1,5 +1,6 @@
 package ar.edu.unlam.pb2;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -14,5 +15,12 @@ public class GerenteTest {
 		assertTrue(miGerente instanceof Empleado);
 	}
 	
-	
+	@Test
+	public void cuandoBrindaDetalleDevuelveQueEsUnGerente() {
+		
+		Empleado gerente = new Gerente();
+		
+		assertEquals("Soy un gerente", gerente.brindarDetalles());
+		
+	}
 }
--- src/ar/edu/unlam/pb2/Gerente.java
+++ src/ar/edu/unlam/pb2/Gerente.java
@@ -1,5 +1,10 @@
 package ar.edu.unlam.pb2;
 
 public class Gerente extends Empleado {
+	
+	@Override
+	public String brindarDetalles() {
+		return "Soy un gerente";
+	}
 
 }
